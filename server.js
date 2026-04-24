bc449bb406c3d93ad6a752b85788683e78f0d7f4
Vijay Prakash <vijay.prakash@swiveIt.com>
2026-04-24 17:57:51 +0700

    change in js

--- server.js
+++ server.js
@@ -93,14 +93,25 @@ app.delete("/api/rooms/:id", (req, res) => {
 
 // ================= BOOKINGS =================
 
-app.get("/api/rooms", (req, res) => {
-  const data = getData();
-  res.json(data.rooms);
-});
-
+// GET all bookings
 app.get("/api/bookings", (req, res) => {
   const data = getData();
   res.json(data.bookings);
+});
+
+// ADD booking
+app.post("/api/bookings", (req, res) => {
+  const data = getData();
+
+  const newBooking = {
+    id: Date.now(),
+    ...req.body
+  };
+
+  data.bookings.push(newBooking);
+  saveData(data);
+
+  res.json(newBooking);
 });
 
 // UPDATE booking
